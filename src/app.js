89906fb501a40f5aa8a70ea8cb6f0b71450c62ef
Kinga Biernacka <kingabiernacka@op.pl>
2017-11-21 17:58:34 +0100

    refactor

--- src/app.js
+++ src/app.js
@@ -2,59 +2,54 @@
  * Created by Kinga on 18.11.2017.
  */
 
-    document.getElementById('add').addEventListener('click', function() {
+    document.getElementById('form').addEventListener('submit', function(e) {
+        if (e.preventDefault) e.preventDefault();
         let input = document.getElementById('item');
         if (input.value) addItemToDo(input.value);
         input.value = '';
     } );
+    let getTopNode = (node) => {
+        return node.parentNode.parentNode.parentNode;
+    };
 
     let removeItem = function () {
-        let item = this.parentNode.parentNode;
-        item.remove();
+        getTopNode(this).remove();
     };
 
     let completeItem = function () {
-        let item = this.parentNode.parentNode.parentNode;
+        let item = getTopNode(this);
         let button = item.getElementsByClassName('complete');
-
         let id = item.parentNode.id;
-
-        button[0].className = (id === 'todo') ? 'fa fa-check-square complete' : 'fa fa-check complete';
-
-        let target = (id === 'todo') ? document.getElementById('completed'): document.getElementById('todo');
-
+        let target;
+        if (id === 'todo') {
+            button[0].className = 'fa fa-check-square complete';
+            target = document.getElementById('completed');
+        } else {
+            button[0].className = 'fa fa-check complete';
+            target = document.getElementById('todo');
+        }
         item.remove();
         target.appendChild(item);
     };
 
+    let createIconButton = (className, fn) => {
+        let icon = document.createElement('i');
+        icon.className = className;
+        icon.addEventListener('click', fn);
+        return icon;
+    };
+
     let addItemToDo = (text) => {
-
         let list = document.getElementById('todo');
-
         let item = document.createElement('div');
         item.innerText = text;
         item.className = 'justify-content-between d-flex list-item';
-
-
         let buttons = document.createElement('div');
-
-        let remove = document.createElement('i');
-        remove.className = 'fa fa-times';
-        buttons.appendChild(remove);
-
-        remove.addEventListener('click', removeItem);
-
-
-        let complete = document.createElement('i');
-        complete.className = 'fa fa-check complete';
-        buttons.appendChild(complete);
-
-        complete.addEventListener('click', completeItem);
+        buttons.appendChild(createIconButton('fa fa-times', removeItem));
+        buttons.appendChild(createIconButton('fa fa-check complete', completeItem));
         item.appendChild(buttons);
-
         let box = document.createElement('div');
         box.className = 'box';
-
         box.appendChild(item);
         list.appendChild(box);
     };
